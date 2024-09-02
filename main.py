b852d9040ac503ffa6e58dc6b2ad6b62c61af00c
Syart Zahiri <5716549+SyartZahiri@users.noreply.github.com>
2024-09-02 20:12:18 +0200

    update

---
+++ main.py
@@ -0,0 +1,24 @@
+import random
+
+def generate_password(length):
+
+    all_characters = [
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',  
+        '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'  
+    ]
+    
+
+    password = ''.join(random.choice(all_characters) for _ in range(length))
+    
+    return password
+
+if __name__ == "__main__":
+    try:
+        length = int(input("Enter the length of the password: "))
+        password = generate_password(length)
+        print(f"Generiertes Passwort: {password}")
+
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
